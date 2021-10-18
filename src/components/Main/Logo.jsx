8549ea25f56d7c512fe507ad44318d7f09259969
josegassull <88618981+josegassull@users.noreply.github.com>
2021-10-18 15:07:40 -0300

    fixed sizes

--- src/components/Main/Logo.jsx
+++ src/components/Main/Logo.jsx
@@ -1,5 +1,5 @@
 const Logo = (props) => {
-    return <div className={"text-center"}>
+    return <div className={"text-center mt-4"}>
         <img id="anima" src={props.isDarkMode ? process.env.PUBLIC_URL + "/assets/gifs/animaDark.gif" : process.env.PUBLIC_URL + "/assets/gifs/animaLight.gif"} alt="animaGif" />
     </div>
 
--- src/components/Main/ModeButton.jsx
+++ src/components/Main/ModeButton.jsx
@@ -5,7 +5,7 @@ const ModeButton = (props) => {
         props.changeMode(!props.isDarkMode);
     }
 
-    return <div className={"text-center"}>
+    return <div className={"text-center mt-4"}>
         <button className={props.isDarkMode ? "btn btn-light mt-2" : "btn btn-dark mt-2"}
                 onClick={() => changeMode()}>{props.isDarkMode ? "Light" : "Dark"}</button>
     </div>
